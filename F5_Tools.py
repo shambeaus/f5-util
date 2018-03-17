946a7d9d10be37b433ae43a1b450fa8034b4ff00
Stephen Shambeau <shambeaus@gmail.com>
2018-03-16 23:00:48 -0500

    initial

---
+++ F5_Tools.py
@@ -0,0 +1,63 @@
+from f5.bigip import ManagementRoot
+import sys
+
+class F5_Tools(object):
+
+    def create_new_pool(self, mgmt, part, pool_name, pool_members):
+        output = []
+        # Check if there is an existing pool
+        if mgmt.tm.ltm.pools.pool.exists(partition=part, name=pool_name):
+            print('Pool already exists')
+            sys.exit()
+        pool = mgmt.tm.ltm.pools.pool.create(partition=part, name=pool_name, monitor='tcp')
+        # Verify pool was created
+        if mgmt.tm.ltm.pools.pool.exists(partition=part, name=pool_name):
+            print('Successfully created pool : ' + pool_name)
+            # NEED too append this to the output
+            output.append(pool.raw)
+        else:
+            print('something is frigged')
+        # Add members to pool
+        print('Adding members to pools')
+        if pool_members:
+            for member in pool_members:
+                pool_member = pool.members_s.members.create(partition=part, name=member)
+                print(' Added member ' + member)
+                output.append(pool_member.raw)         
+        else:
+            print('no members provided')
+            return output
+            sys.exit()
+        return output
+
+
+    def create_new_vip(self, mgmt, virtuals_data, part, vip_name, destination_IP, pool_name):
+        if mgmt.tm.ltm.virtuals.virtual.exists(partition=part, name=vip_name):
+            print('Virtual already exists')
+            sys.exit()        
+        else:
+            virtual = mgmt.tm.ltm.virtuals.virtual.create(partition=part, name=vip_name, ipProtocol='tcp', pool=pool_name, destination=destination_IP)
+        if mgmt.tm.ltm.virtuals.virtual.exists(partition=part, name=vip_name):
+            print('Successfully created VIP : ' + vip_name)
+            output = virtual.raw
+            return output
+        else:
+            print('something is frigged')
+
+    def qc_vip(self, virtuals_data, pools_data, vip_name, destination_IP, pool_name):
+        for line in virtuals_data:
+            if vip_name in line.name:
+                print('VIP ' + vip_name + ' already exists')
+                sys.exit()
+            elif destination_IP in line.destination:
+                print('Destination IP ' + destination_IP + ' already in use.')
+                sys.exit()
+        for line in pools_data:
+            if pool_name in line.name:
+                print('Pool ' + pool_name + ' already in use')
+                sys.exit()
+
+
+
+#    def update_pool_members(self, mgmt, pool_name, pool_members):
+        
---
+++ f5_util.py
@@ -0,0 +1,40 @@
+from f5.bigip import ManagementRoot
+import sys
+from F5_Tools import F5_Tools
+import requests
+import pprint
+
+
+mgmt = ManagementRoot("192.168.109.130", "admin", "pass")
+
+
+partition = 'Common'
+pool_members = ['192.168.5.5:80', '192.168.12.5:80']
+public_IP = '12.7.81.55'
+nat_IP = '192.168.51.5'
+port = '80'
+vip_name = 'VS-{}-{}'.format(public_IP,port)
+destination_IP = '{}:{}'.format(nat_IP,port)
+pool_name = 'POOL-{}-{}'.format(public_IP,port)
+
+virtuals_data = mgmt.tm.ltm.virtuals.get_collection()
+pools_data = mgmt.tm.ltm.pools.get_collection()
+
+
+f5_tools = F5_Tools()
+
+
+
+
+#Create new VIP
+
+f5_tools.qc_vip(virtuals_data, pools_data, vip_name, destination_IP, pool_name)
+
+pooloutput = f5_tools.create_new_pool(mgmt, partition, pool_name, pool_members)
+vipoutput = f5_tools.create_new_vip(mgmt, virtuals_data, partition, vip_name, destination_IP, pool_name)
+
+
+
+
+#pprint.pprint(pooloutput)
+#pprint.pprint(vipoutput)
